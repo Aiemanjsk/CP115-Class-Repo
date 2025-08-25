f1d1b20db53eac593595d70c9f7a883032441384
Aiemanjsk <aiemanmatlin9997@gmail.com>
2025-08-25 13:10:21 +0800

    changes to print lab 8

---
+++ labs/lab08/exercise2.py
@@ -0,0 +1,2 @@
+totalbill = ((3*25 + 2*25 + 4*8) * 110 / 100 + 5) // 6
+print(f"the amount each person needs to pay = {totalbill}")
---
+++ labs/lab08/exercise3/student_grades.py
@@ -0,0 +1,24 @@
+grade1 = 78 
+grade2 = 85
+grade3 = 92
+grade4 = 67
+grade5 = 88
+total_points = grade1 + grade2 + grade3 + grade4 + grade5
+average_score = total_points / 5
+percentage_grade1 = (grade1 / 410) * 100
+percentage_grade2 = (grade2 / 410) * 100
+percentage_grade3 = (grade3 / 410) * 100
+percentage_grade4 = (grade4 / 410) * 100
+percentage_grade5 = (grade5 / 410) * 100
+print("test score 1 " + str (grade1))
+print("test score 2 " + str (grade2))
+print("test score 3 " + str (grade3))
+print("test score 4 " + str (grade4))
+print("test score 5 " + str (grade5))
+print("total grade " + str (total_points))
+print("average student score " + str (average_score))
+print("percentage test 1 " + str(percentage_grade1))
+print("percentage test 2 " + str(percentage_grade2))
+print("percentage test 3 " + str(percentage_grade3))
+print("percentage test 4 " + str(percentage_grade4))
+print("percentage test 5 " + str(percentage_grade5))
