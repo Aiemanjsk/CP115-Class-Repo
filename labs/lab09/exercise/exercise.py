7f318ef45f7cd893a5caa8ce40ca53e8e5f60ddc
Aiemanjsk <aiemanmatlin9997@gmail.com>
2025-09-08 13:14:56 +0800

    lab 9

---
+++ labs/lab09/exercise/exercise.py
@@ -0,0 +1,11 @@
+# elif positioning - can have multiple elif statements
+student_grade = 85
+
+if student_grade >= 90:
+    print("Excellent performance")
+elif student_grade >= 80:    # After if
+    print("Good performance") 
+elif student_grade >= 70:    # After elif
+    print("Satisfactory performance")
+elif student_grade >= 60:    # After elif
+    print("Minimum performance")
--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -4,8 +4,14 @@ overtime_hours = int(input())
 tax_status = input()
 
 # TODO your code here
-
-
+total_income = base_salary * overtime_hours
+if(tax_status = single) and (total_income >= 5000):
+    tax_rate = 22/100
+elif(tax_status = single) and (total_income < 5000):
+    tax_rate = 18/100
+elif(tax_rate = married) and (total_income >= 6000):
+    tax_rate = 20/100
+net_salary = tax_rate * (total_income - (0.115 * total_income))
 print(employee_name)
 print(tax_rate)
 print(f"{net_salary:.2f}")
