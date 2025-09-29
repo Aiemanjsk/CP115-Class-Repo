1ee8119ea76116a2ec2b1b2bf089f9179dae1a8e
Aiemanjsk <aiemanmatlin9997@gmail.com>
2025-09-29 13:14:36 +0800

    change to print lab 11

--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -2,6 +2,27 @@ age = int(input())
 accident_count = int(input())
 
 # Your code here
+base_premium = 0
+if age < 25 :
+    base_premium = base_premium + 2400
+elif age < 50:
+    base_premium = base_premium + 1800
+else :
+    base_premium = base_premium + 2000
+accident_penalty = 0 
+discount = 0
+if accident_count == 0:
+    accident_penalty = accident_penalty + 0
+    discount = discount + 1-10/100
+elif accident_count <= 2:
+    accident_penalty = accident_penalty + 300
+    discount = discount
+else :
+    accident_penalty = accident_penalty + 600
+    discount = discount
+final_premium = accident_penalty + base_premium
+discount_amount = discount * final_premium
+
 
 print(base_premium)
 print(final_premium)
--- labs/lab11/exercise1/exercise1.py
+++ labs/lab11/exercise1/exercise1.py
@@ -2,6 +2,17 @@ num_rounds = int(input())
 
 # TODO: Your code here
 # Use input() inside the loop to get each round's score
+final_score = 0
+rounds_processed = 0
+for rounds_processed in range (0,num_rounds-1):
+    points = float(input())
+    bonus = 20/100
+    if points > 100:
+        points = (points * bonus) + points
+    else:
+        points = points
+
+    final_score += points
 
 print(f"{final_score:.1f}")
 print(rounds_processed)
