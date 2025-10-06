ae26dcc2011b0db145019d2f8bf33ae7413c329e
Aiemanjsk <aiemanmatlin9997@gmail.com>
2025-10-06 13:12:12 +0800

    changes to print lab 12

--- labs/lab12/exercise3/exercise3.py
+++ labs/lab12/exercise3/exercise3.py
@@ -1,6 +1,15 @@
 age_input = input()
 
 # TODO: Your code here
+total_age = 0
+age_count = 0
+average_age = 0
+while age_input != 'done':
+    age = str(input())
+    total_age += age
+    age_count += 1
+    average_age = total_age / age_count
+
 
 print(age_count)
 print(total_age)
