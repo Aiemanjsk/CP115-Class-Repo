fbfad6e58b9afef3dbe3516eff408b28cd6dadfb
Aiemanjsk <aiemanmatlin9997@gmail.com>
2025-10-27 13:10:29 +0800

    changes to print lab 13

--- labs/lab13/exercise1/exercise1.py
+++ labs/lab13/exercise1/exercise1.py
@@ -1,11 +1,12 @@
 correct_password = "python123"
 
 # TODO: Your code here
+login_successful = ''
 attempts_used = 0
 while attempts_used >= 3:
     attempts_used += 1
     if correct_password == "python123":
-        login_successful = "you have succesfull entered"
+        login_successful = True
 
 print(login_successful)
 print(attempts_used)
--- labs/lab13/exercise2/exercise2.py
+++ labs/lab13/exercise2/exercise2.py
@@ -2,9 +2,9 @@
 number = 1
 while number < 101:
     number += 1
-    if number % 7 and number % 13 == 0:
+    if number % 7 == 0 and number % 13 == 0:
         break
-    found_number = number
+    found_number = number + 1
 
     
 print(found_number)
